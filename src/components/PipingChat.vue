8b78c8afd3a5372613b7836fed6f4cd8c80aff6d
Ryo Ota <nwtgck@gmail.com>
2019-03-31 21:26:17 +0900

    [add] Create perr-get-loop

--- src/components/PipingChat.vue
+++ src/components/PipingChat.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <p>
+      Server: <input type="text" v-model="serverUrl">
+      Peer ID: <input type="text" v-model="peerId" placeholder="e.g. bma">
+      <button v-on:click="connectToPeer()">Connect</button>
+    </p>
+    <hr>
+    <p>
+      Your ID: <input type="text" v-bind:value="talkerId" disabled>
+      <input type="text" placeholder="Your talk">
+      <button>Send</button>
+    </p>
     <!-- History of talks-->
     <div>
       <div v-for="talk in talks" :class='{"me": talk.talkerId === talkerId}'>
@@ -25,8 +36,29 @@ type Talk = {
   content: String
 };
 
+/**
+ * Get random ID
+ * @param len
+ */
+function getRandomId(len: number): string {
+  const alphas  = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const chars   = [...alphas];
+
+  const randomArr = window.crypto.getRandomValues(new Uint32Array(len));
+  return Array.from(randomArr).map(n => chars[n % chars.length]).join('');
+}
+
+function getPath(toId: string, fromId: string): string {
+  return `${toId}-to-${fromId}`;
+}
+
 @Component
 export default class PipingChat extends Vue {
+  // TODO: Hard code
+  serverUrl: string = "https://ppng.ml";
+  peerId: string = "";
+
+  talkerId = getRandomId(3);
   // TODO: Hard code
   talks: Talk[] = [
     {
@@ -36,7 +68,7 @@ export default class PipingChat extends Vue {
     },
     {
       time: new Date(),
-      talkerId: "hoge_id1",
+      talkerId: this.talkerId,
       content: "Hello. How are you?"
     },
     {
@@ -46,11 +78,42 @@ export default class PipingChat extends Vue {
     },
   ];
 
-  talkerId = "hoge_id1";
-
   // TODO: Implement properly
   dateFormat(date: Date): string {
     return date.toString();
+  }
+
+  connectToPeer(): void {
+    // Get-loop of peer's message
+    (async ()=>{
+      const url = `${this.serverUrl}/${getPath(this.peerId, this.talkerId)}`;
+      while(true) {
+        try {
+          console.log(`Getting ${url}...`);
+          const res = await fetch(url, {
+            method: "GET"
+          });
+
+          if(res.body === null) {
+            console.log("ERROR: Body not found");
+            return;
+          }
+
+          const talk: Talk = {
+            time: new Date(),
+            talkerId: this.peerId,
+            content: await res.text()
+          };
+
+          console.log("talk:", talk);
+
+          // Push peer's message
+          this.talks.push(talk);
+        } catch (err) {
+          console.error('Error:', err);
+        }
+      }
+    })();
   }
 
 }
