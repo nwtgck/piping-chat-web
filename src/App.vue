b7ea6e442947a623cdcc43d9d4e0abb8bbd3763a
Ryo Ota <nwtgck@gmail.com>
2019-03-31 20:24:35 +0900

    [change] Create a basic of talk history DOM

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Piping Chat</h1>
+    <PipingChat />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import PipingChat from './components/PipingChat.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    PipingChat,
   },
 })
 export default class App extends Vue {}
@@ -22,7 +22,7 @@ export default class App extends Vue {}
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/PipingChat.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <!-- History of talks-->
+    <div>
+      <div v-for="talk in talks" :class='{"me": talk.talkerId === talkerId}'>
+        <span v-if="talk.talkerId !== talkerId">
+          <b>{{ talk.talkerId }}</b>
+          <span class="time">{{ dateFormat(talk.time )}}</span><br>
+        </span>
+        <span v-if="talk.talkerId === talkerId" class="time">
+          {{ dateFormat(talk.time) }}
+        </span>
+        「{{ talk.content }}」
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+type Talk = {
+  time: Date,
+  talkerId: String,
+  content: String
+};
+
+@Component
+export default class PipingChat extends Vue {
+  // TODO: Hard code
+  talks: Talk[] = [
+    {
+      time: new Date(),
+      talkerId: "hoge_id2",
+      content: "Hi!"
+    },
+    {
+      time: new Date(),
+      talkerId: "hoge_id1",
+      content: "Hello. How are you?"
+    },
+    {
+      time: new Date(),
+      talkerId: "hoge_id2",
+      content: "Good."
+    },
+  ];
+
+  talkerId = "hoge_id1";
+
+  // TODO: Implement properly
+  dateFormat(date: Date): string {
+    return date.toString();
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.me{
+  text-align: right;
+}
+.time{
+  font-size: 0.6em;
+  color: #aaa;
+};
+</style>
