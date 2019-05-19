bc10b19faf62fe1c7171de7b00017a4d8722aae5
Ryo Ota <nwtgck@gmail.com>
2019-05-19 11:23:58 +0900

    [change] Remove GitHub ribbon and add GitHub icon

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 // (from: https://vuetifyjs.com/en/framework/icons#icons)
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+// (from: https://e-joint.jp/474/)
+import '@fortawesome/fontawesome';
+// import '@fortawesome/fontawesome-free-solid';
+// import '@fortawesome/fontawesome-free-regular';
+import '@fortawesome/fontawesome-free-brands';
+
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
